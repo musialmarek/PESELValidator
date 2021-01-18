443a8158f987d868dab3bb939e078d0729ec91f7
Marek Musiał <mmusialmarek@gmail.com>
2021-01-18 16:50:53 +0100

    initial commit

---
+++ src/main/java/it/musialmarek/PESELValidator/PeselValidatorApplication.java
@@ -0,0 +1,13 @@
+package it.musialmarek.PESELValidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeselValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeselValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/musialmarek/PESELValidator/PeselValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package it.musialmarek.PESELValidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeselValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
