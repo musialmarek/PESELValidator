ba20e51270e326359648dfaba8adff5098d5dbbe
Marek Musiał <mmusialmarek@gmail.com>
2021-01-21 18:18:23 +0100

    updated Validator to check date from PESEL

--- src/main/java/it/musialmarek/PESELValidator/model/CorrectDatePESELValidator.java
+++ src/main/java/it/musialmarek/PESELValidator/model/CorrectDatePESELValidator.java
@@ -4,6 +4,8 @@ import it.musialmarek.PESELValidator.CorrectDatePESEL;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.time.DateTimeException;
+import java.time.LocalDate;
 
 public class CorrectDatePESELValidator implements ConstraintValidator<CorrectDatePESEL, String> {
     @Override
@@ -15,6 +17,29 @@ public class CorrectDatePESELValidator implements ConstraintValidator<CorrectDat
     public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
         int month = Integer.parseInt(value.substring(2, 4));
         int day = Integer.parseInt(value.substring(4, 6));
+        int year=0;
+        if(month<=12){
+            year = Integer.parseInt(value.substring(0,2))+1900;
+
+        }else if(month<=32){
+            year = Integer.parseInt(value.substring(0,2))+2000;
+            month = month-20;
+        }else if(month<=52){
+            year = Integer.parseInt(value.substring(0,2))+2100;
+            month = month-40;
+        }else if(month<=72){
+            year = Integer.parseInt(value.substring(0,2))+2200;
+            month = month-60;
+        }else if(month<=92){
+            year = Integer.parseInt(value.substring(0,2))+1800;
+            month = month-80;
+        }
+        try {
+            LocalDate date = LocalDate.of(year,month,day);
+        } catch (DateTimeException e) {
+            e.printStackTrace();
+            return false;
+        }
         return month != 0 && day != 0;
     }
 }
