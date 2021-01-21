2ed33645e9792a4896c46e9502152b331e901c67
Marek Musiał <mmusialmarek@gmail.com>
2021-01-21 17:37:49 +0100

    added validation annotation for lenght of PESEL

--- src/main/java/it/musialmarek/PESELValidator/model/PESEL.java
+++ src/main/java/it/musialmarek/PESELValidator/model/PESEL.java
@@ -1,13 +1,16 @@
 package it.musialmarek.PESELValidator.model;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 public class PESEL {
     @NotBlank
+    @Length(min = 11, message = "za krótki")
+    @Size(max = 11, message = "za długi")
     @org.hibernate.validator.constraints.pl.PESEL
     private String number;
 }
