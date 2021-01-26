56b96422f5daaeb6fe19788ff0158936c2a326f8
Marek Musiał <mmusialmarek@gmail.com>
2021-01-26 19:44:02 +0100

    added validation annotation to field number in PESEL

--- src/main/java/it/musialmarek/PESELValidator/model/PESEL.java
+++ src/main/java/it/musialmarek/PESELValidator/model/PESEL.java
@@ -1,16 +1,22 @@
 package it.musialmarek.PESELValidator.model;
 
+import it.musialmarek.PESELValidator.CorrectDatePESEL;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PESEL {
     @NotBlank
     @Length(min = 11, message = "za krótki")
     @Size(max = 11, message = "za długi")
     @org.hibernate.validator.constraints.pl.PESEL
+    @CorrectDatePESEL(message = "zła data")
     private String number;
 }
